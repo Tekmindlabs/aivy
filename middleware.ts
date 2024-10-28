bc2a24bc662facc8255c758d1f7896fe6535d4b1
Tekmindlabs <150348873+Tekmindlabs@users.noreply.github.com>
2024-10-28 14:38:11 +0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get('morphic_session')?.value
+  const token = request.cookies.get('Aivy_session')?.value
 
   if (!token) {
     const url = new URL('/auth/login', request.url)
